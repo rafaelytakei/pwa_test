e64fcbc7e8455ccbef63c7ef0f9763a60d8eadca
Rafael Takei <rafael@suthub.com>
2020-06-24 21:00:22 -0300

    Remove leftovers

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -8,19 +8,6 @@ import buildNavBar from 'Modules/navbar';
 
 /* import $ from 'jquery'; */
 
-if ('serviceWorker' in navigator) {
-	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('/sw.js').then(
-			(registration) => {
-				console.log(`Service Worker registered! Scope: ${registration.scope}`);
-			},
-			(error) => {
-				console.error(`Service Worker registration failed: ${error}`);
-			}
-		);
-	});
-}
-
 window.addEventListener('DOMContentLoaded', () => {
 	buildNavBar();
 	console.log('Hello!');
